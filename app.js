fdb2ad1fe8431322cf9443f3e41d9bc99bb4e775
Wesley <wesleyrasada@gmail.com>
2017-02-25 22:29:57 -0800

    Fixed app.listen

--- app.js
+++ app.js
@@ -48,6 +48,6 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(process.env.PORT || 3000, process.env.IP, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("Yelp Camp has started on port 3000");
 });
