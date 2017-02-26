1190e42afd7cf5be5e26ad56ac591d5c477558ab
Wesley <wesleyrasada@gmail.com>
2017-02-25 23:59:39 -0800

    Fixed app.listen

--- app.js
+++ app.js
@@ -49,4 +49,4 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, process.env.IP);
